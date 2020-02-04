c9bbd3b78a83bd0f2cc9a52207bccd6ec8210402
Will-Ngu <wn7wk@virignia.edu>
2020-02-04 18:32:30 -0500

    corrected models

--- egg/app/models.py
+++ egg/app/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 
 class User(models.Model):
-    user_id = models.CharField(max_length=20)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=14)
+    user_id = models.IntegerField(primary_key = True)
+    email = models.CharField(max_length = 50, unique=True)
+    username = models.CharField(max_length = 50, unique = True)
+    password = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 14)
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    is_deleted = models.BooleanField(False)
     
-    #TODO : Default CreditCard option, Default Shipping Address, favorites/wishlist, email-unique, First & Last name, deleted bool
-    #TODO : Address : addr id, user id, first and last name, addr1, addr2, post code, city, phone
+#class Address(models.Model):
+#    addr id, user id, first and last name, addr1, addr2, post code, city, phone
 
 class Manufacturer(models.Model):
-    man_id = models.CharField(max_length=20)
+    man_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=50)
     web_url = models.CharField(max_length=100)
     phone_num = models.CharField(max_length=14)
 
 class Product(models.Model):
-    product_id = models.CharField(max_length=50)
-    product_type = models.CharField(max_length=50)
+    product_id = models.IntegerField(primary_key = True)
+    product_type = models.CharField(max_length=15)
     man_id = models.CharField(max_length=20)
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
     price = models.IntegerField()
     warrenty = models.IntegerField()
--- egg/egg/urls.py
+++ egg/egg/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.models import models
+from app import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #/api/v1/products/
+    path('api/v1/products', views.Products),
+    #/api/v1/products/id
+    #/api/v1/products/create
+    #/api/v1/products/id/delete
 ]
