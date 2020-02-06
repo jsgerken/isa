c525f38a42ec1ac18635402645992d2f13435107
Will-Ngu <wn7wk@virignia.edu>
2020-02-06 02:33:22 -0500

    urls complete prod

--- egg/app/urls.py
+++ egg/app/urls.py
@@ -6,15 +6,13 @@ from app import views
 from . import views 
 
 urlpatterns = [
-    path('api/v1/products', views.Products),
-    #/api/v1/products/id
-    #/api/v1/products/create
-    #/api/v1/products/id/delete
-]
-
-urlpatterns = [
     path('api/v1/manufacturers/', views.get_all_manufacturers),
     path('api/v1/manufacturers/<int:id>', views.get_or_update_manufacturer),
     path('api/v1/manufacturers/<int:id>/delete/', views.delete_manufacturer),
     path('api/v1/manufacturers/create/', views.create_manufacturer),
+
+    path('api/v1/products/', views.get_all_products),
+    path('api/v1/products/<int:id>', views.get_or_update_product),
+    path('api/v1/products/<int:id>/delete/', views.delete_product),
+    path('api/v1/products/create/', views.create_product),
 ]
