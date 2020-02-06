d895ff9d701a119be43fe9a6ecaf3e671eb3af6d
Will-Ngu <wn7wk@virignia.edu>
2020-02-06 01:30:13 -0500

    commited stash

--- egg/app/urls.py
+++ egg/app/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
-from app.models import models
+#from app.models import models
 from app import views 
 from . import views 
 
--- egg/egg/urls.py
+++ egg/egg/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include
-from app.models import models
+#from app.models import models
 from app import views 
-=======
-from django.urls import include, path
->>>>>>> master
 
 urlpatterns = [
     path('admin/', admin.site.urls),
