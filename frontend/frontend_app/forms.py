a51f2b89dabf8473ff1401424e4afe634870aff8
JeffreyGerken <jsg4t@virginia.edu>
2020-03-23 00:29:59 -0400

    Fixed create account styling

--- frontend/frontend_app/forms.py
+++ frontend/frontend_app/forms.py
@@ -2,15 +2,15 @@ from django import forms
 
 
 class ListingForm(forms.Form):
-    name = forms.CharField(max_length=200, label='', widget=forms.TextInput(
+    name = forms.CharField(max_length=200, widget=forms.TextInput(
         attrs={'placeholder': 'Name:'}))
-    type = forms.CharField(max_length=50, label='', widget=forms.TextInput(
+    type = forms.CharField(max_length=50, widget=forms.TextInput(
         attrs={'placeholder': 'Type:'}))
-    description = forms.CharField(max_length=2000, label='', widget=forms.TextInput(
+    description = forms.CharField(max_length=2000, widget=forms.TextInput(
         attrs={'placeholder': 'Description:'}))
-    price = forms.IntegerField(label='', widget=forms.TextInput(
+    price = forms.IntegerField(widget=forms.NumberInput(
         attrs={'placeholder': 'Price:'}))
-    warranty = forms.CharField(max_length=50, label='', widget=forms.TextInput(
+    warranty = forms.CharField(max_length=50, widget=forms.TextInput(
         attrs={'placeholder': 'Warranty:'}))
-    img_url = forms.CharField(max_length=500, label='', widget=forms.TextInput(
+    img_url = forms.CharField(max_length=500, label="Image URL", widget=forms.TextInput(
         attrs={'placeholder': 'Image URL:'}))
