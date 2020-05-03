8bd593427a4b913aa51e80454b617c1e88def6a1
JeffreyGerken <jsg4t@virginia.edu>
2020-05-02 23:54:19 -0400

    Added shell script to setup db

---
+++ db_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mysql -uroot -p'$3cureUS' -h db
+create user 'www'@'%' identified with mysql_native_password by '$3cureUS';
+create database cs4501 character set utf8;
+grant all on cs4501.* to 'www'@'%';
+quit
