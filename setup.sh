de483424b4beee6bff6919905e9f2de865905e3f
JeffreyGerken <jsg4t@virginia.edu>
2020-05-04 21:30:40 -0400

    Travis

--- setup.sh
+++ setup.sh
@@ -3,8 +3,5 @@ docker pull mysql:latest
 mkdir db
 docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:latest
 sleep 45
-docker exec -i mysql mysql -uroot -p'$3cureUS' -e "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';"
-docker exec -i mysql mysql -uroot -p'$3cureUS' -e "create database cs4501 character set utf8;"
-docker exec -i mysql mysql -uroot -p'$3cureUS' -e "grant all on *.* to 'www'@'%';"
-docker exec -i mysql mysql -uroot -p'$3cureUS' -e "flush privileges;"
+docker exec -i mysql mysql -uroot -p'$3cureUS' -e "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';flush privileges;"
 sleep 20
