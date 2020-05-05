761a87b430c0a91eccc43762b4a63d1ca6955b50
Jacobo <JP3DH@virginia.edu>
2020-05-04 22:41:06 -0400

    env to check travis is running

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker pull mysql:latest
 mkdir db
-docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:latest
+docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:5.7
 sleep 45
-docker run -i -d --name mysql-cmdline --link mysql:db mysql:latest mysql -uroot -p'$3cureUS' -h db -e "create database cs4501 character set utf8;create user 'www'@'%' identified with mysql_native_password by '$3cureUS';grant all on *.* to 'www'@'%';flush privileges;" >> install.log 2>&1
+docker run -i -d --name mysql-cmdline --link mysql:db mysql:5.7 mysql -uroot -p'$3cureUS' -h db -e "create database cs4501 character set utf8;create user 'www'@'%' identified with mysql_native_password by '$3cureUS';grant all on *.* to 'www'@'%';flush privileges;" >> install.log 2>&1
 docker exec -i mysql-cmdline /bin/bash -c "cat install.log"
 sleep 20
