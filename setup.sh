741a636a5a02df755c9b96625f9deb8c6a7ed05a
Jacobo <JP3DH@virginia.edu>
2020-05-04 23:02:57 -0400

    removed setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker pull mysql:latest
+docker pull mysql:5.7
 mkdir db
 docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:5.7
 sleep 45
