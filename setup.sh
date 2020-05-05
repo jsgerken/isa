6b9f864ea4c2ba9ee2d469a5f15b2802c24d4f9f
Jacobo <JP3DH@virginia.edu>
2020-05-05 00:15:41 -0400

    sleeping after composing up

--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@ docker pull mysql:5.7
 mkdir db
 docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:5.7
 sleep 45
-docker run -i -d --name mysql-cmdline --link mysql:db mysql:5.7 mysql -uroot -p'$3cureUS' -h db -e "create database cs4501 character set utf8;create user 'www'@'%' identified with mysql_native_password by '$3cureUS';grant all on *.* to 'www'@'%';flush privileges;"
-sleep 20
