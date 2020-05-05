2bd353e3d14afdb84f6d388ed42bbc837dccb785
JeffreyGerken <jsg4t@virginia.edu>
2020-05-04 20:23:52 -0400

    Travis

--- setup.sh
+++ setup.sh
@@ -4,5 +4,13 @@ mkdir -p ~/cs4501/db
 docker run --name mysql -d -e MYSQL_ROOT_PASSWORD='$3cureUS' -v ~/cs4501/db:/var/lib/mysql mysql:latest
 sleep 20
 docker logs mysql
-docker exec -i mysql mysql -uroot -p'$3cureUS' <<< "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';"
+docker exec -i mysql mysql -uroot -p'$3cureUS' -se "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';" &> /dev/null
+sleep 5
+docker exec -i mysql mysql -uroot -p'$3cureUS' -se "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';" &> /dev/null
+sleep 5
+docker exec -i mysql mysql -uroot -p'$3cureUS' -se "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';" &> /dev/null
+sleep 5
+docker exec -i mysql mysql -uroot -p'$3cureUS' -se "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';" &> /dev/null
+sleep 5
+docker exec -i mysql mysql -uroot -p'$3cureUS' -se "create user 'www'@'%' identified with mysql_native_password by '$3cureUS';create database cs4501 character set utf8;grant all on *.* to 'www'@'%';" &> /dev/null
 sleep 20
